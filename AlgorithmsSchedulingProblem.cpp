e25a9026620a0699c99648ffc93fd304909c2538
Jhoshua Ampo <ampojhoshua@gmail.com>
2023-10-02 17:56:13 -0700

    Add file read

--- AlgorithmsSchedulingProblem.cpp
+++ AlgorithmsSchedulingProblem.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World\n";
+	ifstream InputFile("Input.txt");
+	vector<string> busyScheduleString;
+	vector<string> workingHoursString;
+	string finalWorkingHours;
+	int durationOfMeeting = 0;
+	string inputText;
+
+	for (int i = 0; i < 5; i++)
+	{
+		getline(InputFile, inputText);
+		if (i == 0 || i == 2) { busyScheduleString.push_back(inputText); }
+		if (i == 1 || i == 3) { workingHoursString.push_back(inputText); }
+		if (i == 4) { durationOfMeeting = stoi(inputText); }
+	}
+
+	if
 }
+
+// Read file
+// Store data: P1_schedule, p1_DailyAct, p2_schedule, p2_dailyAct, duration
+// Set daily limits based on both people daily acts
+// 
+//  Compare p1Schedule and p2schedule
+//  Combine blocks together
+//  
